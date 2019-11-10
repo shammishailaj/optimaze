1463adc334abf9b4a897189e45353c816abdebc0
Shammi Shailaj <shammishailaj@gmail.com>
2019-11-10 12:56:41 +0530

    - fix for illegal condition while deducing file size - aaded code to calculate human readable file size

--- main.go
+++ main.go
@@ -58,60 +58,67 @@ func main() {
 		//fmt.Println(f.Name())
 		input := "./"
 		input += f.Name()
-		if !strings.Contains(input, ".jpg") {
-			continue
+		if strings.Contains(input, ".jpg") {
+			fmt.Println(input)
+
+			imwInt, imhInt, size := getFileInfo(input)
+			imw := uint(imwInt)
+			imh := uint(imhInt)
+
+			switch op {
+			case "h":
+				width = imw / 2
+				height = imh / 2
+			case "q":
+				width = imw / 4
+				height = imh / 4
+			default:
+				width = imw
+				height = imh
+			}
+
+			file, err := os.Open(input)
+			if err != nil {
+				log.Println(err)
+			}
+
+			// decode jpeg into image.Image
+			img, err := jpeg.Decode(file)
+			if err != nil {
+				log.Println(err)
+			}
+
+			file.Close()
+
+			log.Println("Input:", input,"Width:", width, "Height:", height, "Size:", ByteCountSI(size))
+
+			var opt jpeg.Options
+
+			opt.Quality = quality
+
+			// resize to width 1000 using Lanczos resampling
+			// and preserve aspect ratio
+			m := resize.Resize(width, height, img, resize.Lanczos3)
+
+			output := "./"
+			output += f.Name()
+
+			out, err := os.Create(output)
+			if err != nil {
+				log.Println(err)
+			}
+			defer out.Close()
+			// write new image to file
+			jpeg.Encode(out, m, &opt)
+			var outsize int64 = 0
+			outstat, outstatErr := out.Stat()
+			if outstatErr == nil {
+				outsize = outstat.Size()
+			}
+			log.Println("Output:", output, "Width:", width, "Height:", height, "Size:", ByteCountSI(outsize))
+		} else {
+			log.Printf("Filename is: %s. Skipping", input)
 		}
-		fmt.Println(input)
-
-		imwInt, imhInt, size := getFileInfo(input)
-		imw := uint(imwInt)
-		imh := uint(imhInt)
-
-		switch op {
-		case "h":
-			width = imw / 2
-			height = imh / 2
-		case "q":
-			width = imw / 4
-			height = imh / 4
-		default:
-			width = imw
-			height = imh
-		}
-
-		file, err := os.Open(input)
-		if err != nil {
-			log.Println(err)
-		}
-
-		// decode jpeg into image.Image
-		img, err := jpeg.Decode(file)
-		if err != nil {
-			log.Println(err)
-		}
-
-		file.Close()
-
-		fmt.Println("Width:", width, "Height:", height, "Size:", size)
-
-		var opt jpeg.Options
-
-		opt.Quality = quality
-
-		// resize to width 1000 using Lanczos resampling
-		// and preserve aspect ratio
-		m := resize.Resize(width, height, img, resize.Lanczos3)
-
-		output := "./"
-		output += f.Name()
-
-		out, err := os.Create(output)
-		if err != nil {
-			log.Println(err)
-		}
-		defer out.Close()
-		// write new image to file
-		jpeg.Encode(out, m, &opt)
 	}
 
 }
@@ -124,7 +131,7 @@ func getFileInfo(imgPath string) (int, int, int64) {
 
 	stats, serr := file.Stat()
 	var size int64 = 0
-	if serr != nil {
+	if serr == nil {
 		size = stats.Size()
 	}
 
@@ -133,4 +140,34 @@ func getFileInfo(imgPath string) (int, int, int64) {
 		log.Println(err)
 	}
 	return im.Width, im.Height, size
+}
+
+// See https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+// See https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
+func ByteCountIEC(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB",
+		float64(b)/float64(div), "KMGTPE"[exp])
 }
